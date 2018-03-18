e09573c089127bd4a7875e484bfe63633a394f2c
ChrisD56 <christopher_D9@hotmail.co.uk>
2018-03-18 20:31:04 +0000

    First commit

---
+++ DiceRoll/Source.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+#include'DiceRoll.h'
+using namespace std;
+
+
+int main() {
+
+	int NoOfPlayers;
+	cout << "Select the number of desired players" << endl;
+	cin >> NoOfPlayers;
+	cout << "You have selected " << NoOfPlayers << "for each team" << endl;
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+
+
+
+}
+
